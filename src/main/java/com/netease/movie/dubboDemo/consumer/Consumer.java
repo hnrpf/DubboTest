48face08049614c5174ef12899744a7a7a9e18c7
hnrpf <fu.wei.2000@163.com>
2015-10-22 17:05:48 +0800

    first commit

---
+++ src/main/java/com/netease/movie/dubboDemo/consumer/Consumer.java
@@ -0,0 +1,31 @@
+package com.netease.movie.dubboDemo.consumer;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.netease.movie.dubboDemo.provider.DemoService;
+
+public class Consumer {
+
+	
+	public static void main(String[] args) throws Exception {  
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(  
+                new String[] { "applicationContext.xml" });  
+        context.start();  
+  
+        DemoService demoService = (DemoService) context.getBean("demoService"); //  
+        String hello = demoService.sayHello("tom"); // ִ  
+        System.out.println(hello); //   
+  
+        //   
+        List list = demoService.getUsers();  
+        if (list != null && list.size() > 0) {  
+            for (int i = 0; i < list.size(); i++) {  
+                System.out.println(list.get(i));  
+            }  
+        }  
+        // System.out.println(demoService.hehe());  
+        System.in.read();  
+    }  
+}
---
+++ src/main/java/com/netease/movie/dubboDemo/provider/DemoService.java
@@ -0,0 +1,10 @@
+package com.netease.movie.dubboDemo.provider;
+
+import java.util.List;
+
+public interface DemoService {
+
+	 public String sayHello(String name);  
+	  
+	 public List getUsers();  
+}
